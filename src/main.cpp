a5fdc4c673ff3f50c5683ba5ea29f08e43f8791f
ravikamat <71521562+ravikamat@users.noreply.github.com>
2026-09-13 22:31:25 +0530

    Mobile edit: main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
-
-
-// ESP32 Feature Generator for: Write program to draw circle
+// ESP32 Firmware Implementation for: Code to draw circle
 #include <Arduino.h>
 
-void handleEsp32Feature() {
-  // Implementation for Write program to draw circle
-  Serial.println("Executing Write program to draw circle");
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Executing: Code to draw circle");
+}
+
+void loop() {
+  // Code loop for Code to draw circle
+
+  delay(1000);
 }
